83b1cf924f22790e5dc7b22a51f63b77bcc450de
LotuxPunk <clement@vandendaelen.com>
2017-12-06 14:56:13 +0100

    up 1

--- da/scripts/app.js
+++ da/scripts/app.js
@@ -215,3 +215,32 @@ function createDownloadLink(href, name) {
     downloadLink.style.display = "none";
     return downloadLink;
 }
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    console.log("Code sauvegardé");
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function putCookieContentToInput(){
+    var code = getCookie("input");
+    document.getElementById("input").value = code;
+}
