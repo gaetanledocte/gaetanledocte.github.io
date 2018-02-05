9a9fa6ac8d55a694524d9d73ebd03172aaf8271c
Gaëtan Le Docte <gaetan.ledocte@gmail.com>
2018-02-05 12:11:09 +0100

    Rearrange app.js

--- da/scripts/app.js
+++ da/scripts/app.js
@@ -1,9 +1,43 @@
+// -----------------------------------------------------------------------------
+// References to Document Elements
+// -----------------------------------------------------------------------------
+
+// Welcome Overlay
 var welcomePanel = document.getElementById("welcomePanel");
 var buttonOpen = document.getElementById("buttonOpen");
 var buttonOpenWithExample = document.getElementById("buttonOpenWithExample");
+
+// Editor
 var editorInput = document.getElementById("editorInput");
 var input = document.getElementById("input");
 var output = document.getElementById("output");
+
+// General Buttons
+var buttonNew = document.getElementById("buttonNew");
+var buttonPrint = document.getElementById("buttonPrint");
+var inputFileNameToSaveAs = document.getElementById("inputFileNameToSaveAs");
+var buttonSaveAsFile = document.getElementById("buttonSaveAsFile");
+var buttonSaveInCache = document.getElementById("buttonSaveInCache");
+
+// Input Buttons
+var buttonInsertMain = document.getElementById("buttonInsert__Main");
+var buttonInsertCondition = document.getElementById("buttonInsert__Condition");
+var buttonInsertSwitch = document.getElementById("buttonInsert__Switch");
+var buttonInsertLoop = document.getElementById("buttonInsert__Loop");
+var buttonInsertParagraph = document.getElementById("buttonInsert__Paragraph");
+var buttonInsertModule = document.getElementById("buttonInsert__Module");
+var buttonExtendedFormatting = document.getElementById("buttonExtendedFormatting");
+var buttonDarkTheme = document.getElementById("buttonDarkTheme");// Output Tool Buttons
+
+// Output Buttons
+var buttonReload = document.getElementById("buttonReload");
+var buttonLiveReload = document.getElementById("buttonLiveReload");
+var buttonExtendedFormatting = document.getElementById("buttonExtendedFormatting");
+
+// -----------------------------------------------------------------------------
+// Global Variables
+// -----------------------------------------------------------------------------
+
 var parser = new PseudoCodeParser({
     delimiters: [
         { pattern: /\bperform\b/i, replacement: "╔══ perform", border: "║" },
@@ -19,21 +53,50 @@ var liveReload = true;
 var inputValue = input.value;
 var darkTheme = true;
 
+// -----------------------------------------------------------------------------
+// Definition of Listeners
+// -----------------------------------------------------------------------------
+
+// Window
 window.addEventListener("load", function() {
+    putCacheContentToInput();
+
     if (input.value !== "") {
         welcomePanel.style.display = "none";
     }
+
     drawDiagram();
 });
 
-//CTRL + S
+window.addEventListener("beforeunload", function() {
+    saveContentToCache();
+});
+
+// Document
 document.addEventListener("keydown", function(event) {
-    if ((window.navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey)  && event.keyCode == 83) {
+    if (isSaveKeyboardShortcut(event)) {
         event.preventDefault();
+
         saveContentToCache();
     }
-  }, false);
+});
 
+// Welcome Overlay
+buttonOpen.addEventListener("click", function(event) {
+    event.preventDefault();
+    welcomePanel.style.display = "none";
+    input.focus();
+});
+
+buttonOpenWithExample.addEventListener("click", function(event) {
+    event.preventDefault();
+    welcomePanel.style.display = "none";
+    input.value = "// Exemple\n\n// On souhaite ajouter, dans un tableau, des personnes identifiées par leurs noms\n// et prénoms. Ensuite, on souhaite afficher à l'écran le contenu du tableau.\n\n---* Exemple de diagramme\nnbPers = 0\nObtenir nomPrénom\n\ndo while (nomPrénom != \"ZZZ\")\n   module(AjoutPersonne;tabPersonnes, nbPersonnes, nomPrénom;tabPersonnes, nbPersonnes)\n   Obtenir nomPrénom\nenddo\nparagraphe(Sorties)\n------\n\n---* AjoutPersonne\niPers = 0\ndo while (iPers < nbPers AND tabPers[iPers] != nomPrénom)\n   iPers++\nenddo\nif (iPers = nbPers)\n   tabPers[iPers] = nomPrénom\nelse\n   Sortir nomPrénom, \" déjà présent dans le tableau.\"\nendif\n------\n\n---* Sorties\niPers = 0\ndo while (iPers < nbPers)\n   Sortir tabPers[iPers]\n   iPers++\nenddo\n------";
+    input.focus();
+    drawDiagram();
+});
+
+// Input
 input.addEventListener("keyup", function() {
     if (liveReload && inputValue !== input.value) {
         inputValue = input.value;
@@ -52,30 +115,7 @@ input.addEventListener("keypress", function (event) {
     }
 });
 
-// Welcome Panel
-
-buttonOpen.addEventListener("click", function(event) {
-    event.preventDefault();
-    welcomePanel.style.display = "none";
-    input.focus();
-});
-
-buttonOpenWithExample.addEventListener("click", function(event) {
-    event.preventDefault();
-    welcomePanel.style.display = "none";
-    input.value = "// Exemple\n\n// On souhaite ajouter, dans un tableau, des personnes identifiées par leurs noms\n// et prénoms. Ensuite, on souhaite afficher à l'écran le contenu du tableau.\n\n---* Exemple de diagramme\nnbPers = 0\nObtenir nomPrénom\n\ndo while (nomPrénom != \"ZZZ\")\n   module(AjoutPersonne;tabPersonnes, nbPersonnes, nomPrénom;tabPersonnes, nbPersonnes)\n   Obtenir nomPrénom\nenddo\nparagraphe(Sorties)\n------\n\n---* AjoutPersonne\niPers = 0\ndo while (iPers < nbPers AND tabPers[iPers] != nomPrénom)\n   iPers++\nenddo\nif (iPers = nbPers)\n   tabPers[iPers] = nomPrénom\nelse\n   Sortir nomPrénom, \" déjà présent dans le tableau.\"\nendif\n------\n\n---* Sorties\niPers = 0\ndo while (iPers < nbPers)\n   Sortir tabPers[iPers]\n   iPers++\nenddo\n------";
-    input.focus();
-    drawDiagram();
-});
-
-// General Tool Buttons
-
-var buttonNew = document.getElementById("buttonNew");
-var buttonPrint = document.getElementById("buttonPrint");
-var inputFileNameToSaveAs = document.getElementById("inputFileNameToSaveAs");
-var buttonSaveAsFile = document.getElementById("buttonSaveAsFile");
-var buttonSaveInCache = document.getElementById("buttonSaveInCache");
-
+// General Buttons
 buttonNew.addEventListener("click", function(event){
     event.preventDefault();
     localStorage["codeDA"] = "";
@@ -106,17 +146,7 @@ buttonSaveAsFile.addEventListener("click", function(event){
     }
 });
 
-// Input Tools Buttons
-
-var buttonInsertMain = document.getElementById("buttonInsert__Main");
-var buttonInsertCondition = document.getElementById("buttonInsert__Condition");
-var buttonInsertSwitch = document.getElementById("buttonInsert__Switch");
-var buttonInsertLoop = document.getElementById("buttonInsert__Loop");
-var buttonInsertParagraph = document.getElementById("buttonInsert__Paragraph");
-var buttonInsertModule = document.getElementById("buttonInsert__Module");
-var buttonExtendedFormatting = document.getElementById("buttonExtendedFormatting");
-var buttonDarkTheme = document.getElementById("buttonDarkTheme");
-
+// Input Buttons
 buttonInsertMain.addEventListener("click", function(event) {
     event.preventDefault();
     insert(input, "---*\n\n------");
@@ -165,12 +195,7 @@ buttonDarkTheme.addEventListener("click", function(event) {
     darkTheme = !darkTheme;
 });
 
-// Output Tool Buttons
-
-var buttonReload = document.getElementById("buttonReload");
-var buttonLiveReload = document.getElementById("buttonLiveReload");
-var buttonExtendedFormatting = document.getElementById("buttonExtendedFormatting");
-
+// Output Buttons
 buttonReload.addEventListener("click", function(event) {
     event.preventDefault();
     if (!liveReload) {
@@ -202,7 +227,9 @@ buttonExtendedFormatting.addEventListener("click", function(event) {
     drawDiagram();
 });
 
+// -----------------------------------------------------------------------------
 // Functions
+// -----------------------------------------------------------------------------
 
 function drawDiagram() {
     output.innerHTML = parser.getFormattedDiagram(input.value, useExtendedFormatting);
@@ -246,4 +273,8 @@ function saveContentToCache(){
 function putCacheContentToInput(){
 	var codeDA = localStorage['codeDA'] || '';
 	document.getElementById("input").value = codeDA;
+}
+
+function isSaveKeyboardShortcut(event) {
+    return (window.navigator.platform.match("Mac") ? event.metaKey : event.ctrlKey) && event.keyCode == 83;
 }
